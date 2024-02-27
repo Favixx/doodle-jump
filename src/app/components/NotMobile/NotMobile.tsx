26a02447f9affb8aa9a20e32ea8b23386c5ff88f
Favixx <bogdandom52@gmail.com>
2024-02-27 17:12:27 +0200

    Update NotMobile.tsx

--- src/app/components/NotMobile/NotMobile.tsx
+++ src/app/components/NotMobile/NotMobile.tsx
@@ -3,11 +3,13 @@ import { ScreenContainer } from "../styled/ScreenContainer";
 import styled from "styled-components";
 
 const Heading = styled.h1`
+  font-size: 5vw;
   color: #61dafb;
   margin-bottom: 10px;
 `;
 
 const Avaliability = styled.p`
+  font-size: 2vw;
   color: #fff;
 `;
 
