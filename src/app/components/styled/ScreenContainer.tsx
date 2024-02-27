aec56d2f436398e57b51228ce854bdb21089a538
Sprynskyi Vitaliy <121flame121@gmail.com>
2024-02-27 18:29:20 +0200

    scroll fix

--- src/app/components/styled/ScreenContainer.tsx
+++ src/app/components/styled/ScreenContainer.tsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ScreenContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
+  /* height: 100%; */
 `;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import checkIsMobileDevice from "../utils/isMobileDevice";
-import HomeScreen from "./components/HomeScreen/HomeScreen";
-import NotMobile from "./components/NotMobile/NotMobile";
-import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
+'use client';
+import React, { useState, useEffect } from 'react';
+import checkIsMobileDevice from '../utils/isMobileDevice';
+import HomeScreen from './components/HomeScreen/HomeScreen';
+import NotMobile from './components/NotMobile/NotMobile';
+import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
+import { useViewportHeight } from '@/hooks/useViewportHeight';
 
 export default function Home() {
   const [isMobile, setIsMobile] = useState<boolean>(false);
   const [isInitialized, setIsInitialized] = useState<boolean>(false);
+
+  useViewportHeight();
 
   useEffect(() => {
     const isUsingMobile = () => {
@@ -17,10 +20,10 @@ export default function Home() {
 
     isUsingMobile();
 
-    window.addEventListener("resize", isUsingMobile);
+    window.addEventListener('resize', isUsingMobile);
 
     return () => {
-      window.removeEventListener("resize", isUsingMobile);
+      window.removeEventListener('resize', isUsingMobile);
     };
   }, []);
 
