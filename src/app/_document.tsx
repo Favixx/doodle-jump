c77733d84f34bbcf0aa8def3763f1df08c6935c8
Favixx <bogdandom52@gmail.com>
2024-03-12 22:07:39 +0200

    Create _document.tsx

---
+++ src/app/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+        {/* Ваші метатеги та інші елементи head тут */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
