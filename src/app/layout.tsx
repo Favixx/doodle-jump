664453fa9c267757a08c3b6ed970869c3edda574
Favixx <bogdandom52@gmail.com>
2024-03-12 20:52:44 +0200

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
