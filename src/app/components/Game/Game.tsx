2462241b3722269b04d9125b31d4c3485804f3b0
Favixx <bogdandom52@gmail.com>
2024-04-21 00:19:22 +0300

    Update Game.tsx

--- src/app/components/Game/Game.tsx
+++ src/app/components/Game/Game.tsx
@@ -77,7 +77,7 @@ const Game: React.FC = () => {
                   x={adjustedX}
                   y={adjustedY}
                   images={BUBBLE_FRAMES}
-                  animationSpeed={0.72}
+                  animationSpeed={0.65}
                   loop={false}
                   onComplete={() =>
                     dispatch({
